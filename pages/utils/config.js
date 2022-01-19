8a7e08d63521955f0efaef2146e2e59937d097a7
hanyi <791845283@qq.com>
2022-01-19 17:25:29 +0800

    finish

--- pages/utils/config.js
+++ pages/utils/config.js
@@ -1,10 +1,10 @@
 class Config{
   constructor(){}
 }
-
-Config.debug = true;
-Config.restUrl = Config.debug ? 'http://diancan.com/api/v1/' : "https://diancan.coderyun.cn/api/v1/";
-Config.picUrl = Config.debug ? 'http://diancan.com' : "https://diancan.coderyun.cn";
+//如果需要后端接口地址请联系 加V: 18850737047
+Config.debug = false;
+Config.restUrl = Config.debug ? 'http://diancan.com/api/v1/' : "https://api.coderyun.cn/api/v1/";
+Config.picUrl = Config.debug ? 'http://diancan.com' : "https://api.coderyun.cn";
 
  
     
